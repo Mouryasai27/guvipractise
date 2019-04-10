419da63acb5ce36bfb6996eb8fbe9a186d3fec82
MOURYA SAI CHARAN REDDY KYPA <36075316+Mouryasai27@users.noreply.github.com>
2019-04-10 09:14:17 +0530

    Create primenumbers.py

---
+++ primenumbers.py
@@ -0,0 +1,10 @@
+l,u = list(map(int,input().split()))
+count = 0
+for i in range(l,u+1):
+    if i>1:
+        for j in range(2,i):
+            if i%j == 0:
+                break
+        else:
+            count += 1
+print(count)
